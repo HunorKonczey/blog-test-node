fb7404e0358a1ff35f800e8ab65bcef1aec5cf95
HunorKonczey <hunorkonczey@gmail.com>
2021-01-23 20:10:56 +0200

    -implement login

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,6 +9,16 @@ const handleErrors = (err) => {
     errors.email = 'That email is already registered';
   }
 
+  //incorrect email
+  if (err.message === 'Incorrect email') {
+    errors.email = 'That email is not registered';
+  }
+
+  //incorrect password
+  if (err.message === 'Incorrect password') {
+    errors.password = 'That password is not registered';
+  }
+
   if (err.message.includes('User validation failed')) {
     Object.values(err.errors).forEach(({ properties }) => {
       errors[properties.path] = properties.message;
@@ -50,7 +60,19 @@ const signup_post = async (req, res) => {
   }
 };
 
-const login_post = (req, res) => {};
+const login_post = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+    const token = createToken(user._id);
+    res.cookie('jwt', token, { httpOnly: true, maxAge: jwtExpire * 1000 });
+    res.status(201).json({ user: user._id });
+  } catch (err) {
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
+  }
+};
 
 module.exports = {
   signup_get,
